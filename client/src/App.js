5b95dd8202edd5786e47fec71b344359226d06d7
NGUD-0119 <uni0119@proton.me>
2023-02-11 05:27:15 -0500

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+
+  const [url, setURL] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ url });
+    setURL("");
+  };
+
   return (
-    <div className="App">
-      <p>Hello World</p>
+    <div className="home">
+      <form className='home_form'>
+        <h2>ChatGPT</h2>
+        <label>Provide the ChatGPT Url</label>
+        <input
+          type= 'url'
+          name= 'url'
+          id= 'url'
+          value={url}
+          onChange = {(e) => setURL(e.target.value)}
+        />
+        <button onClick={handleSubmit}>ADD Website</button>
+      </form>
     </div>
   );
 }
